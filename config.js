df4f356c15503e3f8a2390e7e860be38c43a3ad4
q3mrs <q3mrss@gmail.com>
2026-03-12 20:47:13 +0300

    Update

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+  challenge: false, // Set to true if you want to enable password protection.
+  users: {
+    // You can add multiple users by doing username: 'password'.
+    interstellar: "password",
+  },
+  bare: "https://bare.benroberts.dev/",
+};
+
+export default config;
