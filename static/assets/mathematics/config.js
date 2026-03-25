4f18524cda21d193176f0671460f810e0ea67ecc
q3mrs <123552016+q3mrs@users.noreply.github.com>
2026-03-25 23:49:56 +0300

    Update config.js

--- static/assets/mathematics/config.js
+++ static/assets/mathematics/config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
   prefix: "/a/",
-  bare: "/ca/",
-  encodeUrl: Ultraviolet.codec.xor.encode,
-  decodeUrl: Ultraviolet.codec.xor.decode,
-  handler: "/assets/mathematics/handler.js?v=9-30-2024",
-  bundle: "/assets/mathematics/bundle.js?v=9-30-2024",
-  config: "/assets/mathematics/config.js?v=9-30-2024",
-  sw: "/assets/mathematics/sw.js?v=9-30-2024",
+  bare: "/ca/", // kendi railway tünelin
+  encodeUrl: Ultraviolet.codec.base64.encode, // xor yerine base64 daha stabildir
+  decodeUrl: Ultraviolet.codec.base64.decode,
+  handler: "/assets/mathematics/handler.js?v=2026",
+  bundle: "/assets/mathematics/bundle.js?v=2026",
+  config: "/assets/mathematics/config.js?v=2026",
+  sw: "/assets/mathematics/sw.js?v=2026",
 };
